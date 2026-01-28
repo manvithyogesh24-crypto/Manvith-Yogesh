e185ccdb74f2ed665a2a9df42382111888a2d874
Manvith Y <manvithyogesh40@gmail.com>
2026-01-28 16:02:31 +0530

    Add files via upload

---
+++ code3.java
@@ -0,0 +1,13 @@
+package Day2;
+
+public class code3 {
+	public static void main(String[]args) {
+		int a = 10;
+        int b = 20;
+        a = a + b;
+        b = a - b; 
+        a = a - b; 
+        System.out.println("After swapping: a = " + a + ", b = " + b);      
+	}
+
+}
